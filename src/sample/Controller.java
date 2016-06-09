86fd9885341a161d13c1063a26f4c70292b9044f
gohv <hristovsotirovgeorgi@gmail.com>
2016-06-09 10:57:18 +0300

    Method Updated

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,18 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import javax.swing.*;
+
 public class Controller {
+
+    @FXML private TextField nameField;
+
+    public void saySup(ActionEvent actionEvent) {
+
+        JOptionPane.showMessageDialog(null, "SUP " + nameField.getText(), "Whats up?" , JOptionPane.INFORMATION_MESSAGE);
+
+    }
 }
